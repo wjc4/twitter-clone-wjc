50bc99963b10ff7920d21f61fcc1c6b844f1cfc8
wjc4 <R150017@e.ntu.edu.sg>
2019-07-04 12:28:22 +0900

    working sso and basic tweeting

--- src/twitter_clone/db.py
+++ src/twitter_clone/db.py
@@ -11,29 +11,72 @@ class DB:
         except:
             flask.current_app.logger.error("Failed to connect to Redis")
 
-    def create_user(self, username, password):
-        if self.existing_user(username):
+    def create_user(self, username, password, display_name, sso=False):
+        if self.existing_username(username):
             raise Exception("User already exists")
         user_id = str(self.db.incrby("next_user_id", 1000))
         # @TODO hash password
-        self.db.hmset("user:" + user_id, dict(username=username, password=password))
+        self.db.hmset(
+            "user:" + user_id,
+            dict(
+                username=username,
+                password=password,
+                display_name=display_name,
+                sso=str(sso),
+            ),
+        )
         self.db.hset("users", username, user_id)
-        return username
+        return user_id
 
     def get_user_id(self, username):
-        if not self.existing_user(username):
+        if not self.existing_username(username):
             raise Exception("User not found")
         return str(self.db.hget("users", username), "utf-8")
 
-    def existing_user(self, username):
+    def get_username(self, user_id):
+        return str(self.db.hget("user:" + user_id, "username"), "utf-8")
+
+    def get_user(self, user_id):
+        undecoded_user = self.db.hgetall("user:" + str(user_id))
+        user = dict(
+            user_id=user_id,
+            username=undecoded_user[b"username"].decode("utf-8"),
+            display_name=undecoded_user[b"display_name"].decode("utf-8"),
+            sso=bool(undecoded_user[b"sso"].decode("utf-8")),
+        )
+        return user
+
+    def existing_username(self, username):
         return self.db.hexists("users", username)
+
+    def existing_user_id(self, user_id):
+        return self.db.exists("user:" + user_id)
 
     def validate_user(self, username, password):
         # @TODO not hashed yet. pls check
-        stored_hash = str(
-            self.db.hget("user:" + self.get_user_id(username), "password"), "utf-8"
-        )
-        return password == stored_hash
+        # @TODO raise exceptions for login failures. eg. invalid user, invalid password
+        user_id = self.get_user_id(username)
+        sso = bool(self.db.hget("user:" + user_id, "sso"))
+        if sso:
+            raise Exception("Account needs to be logged in through SSO")
+        stored_hash = str(self.db.hget("user:" + user_id, "password"), "utf-8")
+        if password != stored_hash:
+            raise Exception("Wrong Password")
+        return user_id
+
+    def create_google_user(self, google_id, user_id, username):
+        if self.existing_google_id(google_id):
+            raise Exception("Account already exists")
+        self.db.hset("googlers", google_id, user_id)
+        return google_id
+
+    def get_user_id_from_google_id(self, google_id):
+        if not self.existing_google_id(google_id):
+            raise Exception("Google id not found")
+        return str(self.db.hget("googlers", google_id), "utf-8")
+
+    def existing_google_id(self, google_id):
+        return self.db.hexists("googlers", str(google_id))
 
     def existing_post(self, post_id):
         # @TODO check this. its weird
@@ -42,29 +85,44 @@ class DB:
     def get_post(self, post_id):
         if not self.existing_post(post_id):
             raise Exception("Post not found")
-        return self.db.hgetall("post:" + str(post_id, "utf-8"))
+        post = self.db.hgetall("post:" + str(post_id, "utf-8"))
+        return dict(
+            username=post[b"username"].decode("utf-8"),
+            time_stamp=post[b"time_stamp"].decode("utf-8"),
+            text=post[b"text"].decode("utf-8"),
+        )
 
     def get_user_timeline(self, username):
         # return all(?) user's post
         user_id = self.get_user_id(username)
+        display_name = self.get_user(user_id)["display_name"]
         posts = self.db.lrange("posts:" + str(user_id), 0, -1)
         user_posts = []
         for post_id in posts:
             post = self.db.hgetall("post:" + str(post_id, "utf-8"))
             user_posts.append(
-                dict(username=username, time_stamp=post[b"ts"], text=post[b"text"])
+                dict(
+                    username=username,
+                    display_name=display_name,
+                    time_stamp=post[b"time_stamp"].decode("utf-8"),
+                    text=post[b"text"].decode("utf-8"),
+                )
             )
         return user_posts
 
     def get_home_timeline(self, username):
         pass
 
-    def put_user_posts(self, username, text):
+    def get_global_timeline(self):
+        pass
+
+    def put_user_post(self, username, text):
         # only trims timeline currently.
         user_id = self.get_user_id(username)
         post_id = str(self.db.incr("next_post_id"))
         self.db.hmset(
-            "post:" + post_id, dict(user_id=user_id, time_stamp=time.time(), text=text)
+            "post:" + post_id,
+            dict(user_id=user_id, time_stamp=int(time.time()), text=text),
         )
         self.db.lpush("posts:" + str(user_id), str(post_id))
         self.db.lpush("timeline:" + str(user_id), str(post_id))
@@ -79,3 +137,27 @@ class DB:
         self.db.lrem("posts:" + str(user_id), 1, post_id)
         self.db.lrem("timeline:" + str(user_id), 1, post_id)
         # @TODO delete post from followers timeline
+
+    def existing_session(self, session_id):
+        return self.db.exists("session:" + session_id)
+
+    def put_session(self, session_id, user_id):
+        if self.existing_session(session_id):
+            raise Exception("Existing session. Do check for collision")
+        self.db.hmset(
+            "session:" + session_id, dict(user_id=user_id, time_stamp=time.time())
+        )
+        self.db.hset("sessions", session_id, user_id)
+
+    def get_session(self, session_id):
+        if not self.existing_session(session_id):
+            raise Exception("Session not found")
+        # @TODO check for expiration
+        user_id = str(self.db.hget("session:" + session_id, "user_id"), "utf-8")
+        return user_id
+
+    def delete_session(self, session_id):
+        if not self.existing_session(session_id):
+            raise Exception("Session not found")
+        self.db.delete("session:" + session_id)
+        self.db.hdel("sessions", session_id)
--- src/twitter_clone/routes.py
+++ src/twitter_clone/routes.py
@@ -1,8 +1,12 @@
-import logging, sys, os
+import logging, sys, os, uuid, json
 from flask import Flask, session, redirect, url_for, render_template, request
+
+from . import google_auth
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SESSION_SECRET", os.urandom(50))
+
+app.register_blueprint(google_auth.app)
 
 from .db import DB
 
@@ -25,14 +29,44 @@ def home():
 
 @app.route("/login", methods=["GET"])
 def login():
-    return app.send_static_file("login.html")
-    # return render_template("login.html")
+    error = None
+    if is_logged_in():
+        return redirect(url_for("home"))
+    else:
+        return app.send_static_file("login.html")
+        # return render_template("login.html", error=error)
+
+
+@app.route("/redirect", methods=["GET"])
+def redirection():
+    error = None
+    if google_auth.is_logged_in():
+        return check_google_session()
+    if is_logged_in():
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("login"))
+        # return render_template("login.html", error=error)
+
+
+@app.route("/login", methods=["POST"])
+def create_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    try:
+        create_user_session(db.validate_user(username, password))
+    except:
+        # @TODO failed login
+        return "failed login"
+    return redirect(url_for("home"))
 
 
 @app.route("/logout", methods=["GET"])
 def logout():
-    session.pop("username", None)
-    return app.send_static_file("login.html")
+    if "session_id" in session:
+        db.delete_session(session["session_id"])
+    flush_session()
+    return redirect(url_for("login"))
     # return render_template("login.html")
 
 
@@ -46,16 +80,117 @@ def signup():
     app.logger.debug(dict(request.form))
     username = request.form["username"]
     password = request.form["password"]
-    db.create_user(username, password)
-    session["username"] = username
+    display_name = request.form["display_name"]
+    # @TODO catch errors
+    user_id = db.create_user(username, password, display_name)
+
+    create_user_session(user_id)
+
     return redirect(url_for("home"))
 
 
-@app.route("/profile/<username>", methods=["GET"])
+@app.route("/signup2", methods=["GET", "POST"])
+def signup_google():
+    error = None
+    if not google_auth.is_logged_in():
+        return "Error 401: Unauthorised"
+    else:
+        google_id = session["google_id"]
+        app.logger.debug(
+            "checking if gid:"
+            + str(google_id)
+            + " of type "
+            + str(type(google_id))
+            + " exists"
+        )
+        if db.existing_google_id(google_id):
+            app.logger.debug("google_id alr exists")
+            return check_google_session()
+    if request.method == "GET":
+        return app.send_static_file("register_google.html")
+    # @TODO enforce valid username
+    app.logger.debug(dict(request.form))
+    username = request.form["username"]
+    password = request.form["username"]
+    display_name = request.form["display_name"]
+    # @TODO catch errors
+    user_id = db.create_user(username, password, display_name)
+    db.create_google_user(session["google_id"], user_id, username)
+    create_user_session(user_id)
+
+    return redirect(url_for("home"))
+
+
+def create_user_session(user_id):
+    session_id = str(uuid.uuid4())
+    session["session_id"] = session_id
+    session["user_id"] = user_id  # DELETE AFTER DEPRECATION
+    session.permanent = True
+    db.put_session(session_id, user_id)
+
+
+def is_logged_in():
+    status = False
+    if "session_id" in session:
+        status = db.existing_session(session["session_id"])
+        if not status:
+            flush_session()
+    return status
+
+
+def check_google_session():
+    google_id = session["google_id"]
+    if not db.existing_google_id(google_id):
+        return redirect(url_for("signup_google"))
+    if not is_logged_in():
+        user_id = db.get_user_id_from_google_id(google_id)
+        create_user_session(user_id)
+    return redirect(url_for("home"))
+
+
+def flush_session():
+    [session.pop(key) for key in list(session.keys()) if key != "_flashes"]
+
+
+@app.route("/profile", methods=["GET"])
 def profile():
-    pass
+    if not is_logged_in():
+        app.logger.debug("not logged in")
+        return redirect(url_for("login"))
+
+    user_id = db.get_session(session["session_id"])
+    username = db.get_username(user_id)
+
+    timeline = db.get_user_timeline(username)
+    return render_template("profile.html", timeline=timeline)
 
 
 @app.route("/post", methods=["POST"])
 def post():
-    pass
+    if not is_logged_in():
+        return redirect(url_for("login"))
+    session_id = session["session_id"]
+    if not db.existing_session(session_id):
+        return redirect(url_for("logout"))
+    user_id = db.get_session(session["session_id"])
+    username = db.get_username(user_id)
+
+    tweet = request.form["tweet"]
+    referrer = request.referrer
+    db.put_user_post(username, tweet)
+    return redirect(referrer)
+
+
+@app.route("/googleinfo")
+def google_debug():
+    if google_auth.is_logged_in():
+        user_info = google_auth.get_user_info()
+        return (
+            "<div>You are currently logged in as "
+            + user_info["given_name"]
+            + "<div><pre>"
+            + json.dumps(user_info, indent=4)
+            + "</pre>"
+        )
+
+    return "You are not currently logged in."
